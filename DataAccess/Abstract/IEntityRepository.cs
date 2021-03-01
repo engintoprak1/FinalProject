82c118c929239474d795af6f2bd0a7698811a715
Engin Toprak <engin.toprak1@ogr.sakarya.edu.tr>
2021-03-01 19:10:12 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint - generic kısıt
+    //class : referans tip olabilir demek
+    //IEntity : IEntity olabilir veya IEntity implemente nesne olabilir.
+    //new() : new'lenebilir olmalı
+    //interfaceler newlenemez
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        //GetAllByCategory(int categoryId); //ürünleri kategoriye göre filtrele expression ile gitti List<T> 
+    }
+}
